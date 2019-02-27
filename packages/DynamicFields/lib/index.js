35bcf86e53ef733802d184e8f19bebb1d31989bd
Roman Nguyen <march08@outlook.com>
2019-02-27 15:02:26 +0700

    playing with TS

---
+++ packages/DynamicFields/lib/index.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var React = require("react");
+var DynamicFields = function (props) {
+    return React.createElement("h1", null,
+        "Hello, ",
+        props.name);
+};
+DynamicFields.defaultProps = {
+    name: "Guest User",
+};
+exports.default = DynamicFields;
+//# sourceMappingURL=index.js.map
---
+++ packages/docs/src/App.tsx
@@ -0,0 +1,34 @@
+import React, { Component, useState } from 'react';
+import logo from './logo.svg';
+
+import DynamicFields from '@duik/dynamic-fields'
+import './App.css';
+
+const App = () => {
+
+  const [count, setCount] = useState(0);
+
+  return (
+
+    <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+      <button onClick={() => setCount(count + 1)}>{count}</button>
+      <DynamicFields />
+    </header>
+  </div>
+  )
+}
+
+export default App;
