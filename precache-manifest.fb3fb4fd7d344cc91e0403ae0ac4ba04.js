f3faa92d37da658c7425c23f97db75c82a89fce7
Roman Nguyen <march08@outlook.com>
2019-05-20 20:45:42 +0700

    Updates

---
+++ precache-manifest.fb3fb4fd7d344cc91e0403ae0ac4ba04.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce8e6090e27bb224d6a214dd52a876a0",
+    "url": "/duik/index.html"
+  },
+  {
+    "revision": "b6b0f6154b306f063209",
+    "url": "/duik/static/css/main.4c1aa02d.chunk.css"
+  },
+  {
+    "revision": "0444c09fd5e4db2762ba",
+    "url": "/duik/static/js/2.e5b17f50.chunk.js"
+  },
+  {
+    "revision": "b6b0f6154b306f063209",
+    "url": "/duik/static/js/main.d44b6da7.chunk.js"
+  },
+  {
+    "revision": "a8bf8a87640341adaa15",
+    "url": "/duik/static/js/runtime~main.3e650ccd.js"
+  },
+  {
+    "url": "/duik/static/media/073b232b11da9cf8777616dddfed40e7.073b232b.ttf"
+  },
+  {
+    "url": "/duik/static/media/20a6441b2d752ea7ba98a6cea6275a3b.20a6441b.woff"
+  },
+  {
+    "url": "/duik/static/media/6d70aa99c17529538f8fed9fc214eca0.6d70aa99.eot"
+  },
+  {
+    "url": "/duik/static/media/75bd5390ed5a7f0a71dfb433326763e9.75bd5390.svg"
+  }
+]);
