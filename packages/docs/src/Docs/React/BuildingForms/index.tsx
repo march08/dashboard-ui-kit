875801cb0300ce67ac6c7779b16f7af130505fc4
Roman Nguyen <march08@outlook.com>
2019-06-08 16:47:08 +0700

    cleanup docs

--- packages/docs/src/Docs/React/BuildingForms/index.tsx
+++ packages/docs/src/Docs/React/BuildingForms/index.tsx
@@ -103,50 +103,21 @@ export const ReactDocsBuildingTopBar = () => {
 
   return (
     <DocsContentPage>
-
+      {/* 
       <PageContent data={[
         { id: 'simple', label: 'Basic Example' },
         { id: 'menu', label: 'With navigation' },
         { id: 'search', label: 'Extra elements' },
-      ]} />
+      ]} /> */}
       <H1>
-        Building Top Bar
-        </H1>
-      <p>
-        In order to create consitent UI, we are including some handy components for building TopBar in a simplest way possible. Of course, you don't have to limit yourself to these examples, but it's the fastest way to build the top bars that matches the design language of the kit!
-      </p>
-      <p>
-        This page offers some specific examples of how to build your perfect TopBar. Make sure you visit <Link to="/docs/react/building-layout">Building Layout</Link> page as well!
-      </p>
-      <h2>List of related components</h2>
-      <ol>
-        <li><Link to="/docs/react/top-bar">TopBar</Link></li>
-        <li><Link to="/docs/react/top-bar-link">TopBarLink</Link></li>
-        <li><Link to="/docs/react/top-bar-link-container">TopBarLinkContainer</Link></li>
-        <li><Link to="/docs/react/top-bar-section">TopBarSection</Link></li>
-        <li><Link to="/docs/react/top-bar-title">TopBarTitle</Link></li>
-      </ol>
-      <h2 id="simple">Simple TopBar Example</h2>
+        Building Forms
+      </H1>
+      <h2 id="simple">Example Form</h2>
       <PageMock white>
         {simpleExample}
       </PageMock>
       <CodeRH
         content={simpleExample}
-      />
-
-      <h2 id="menu">Menu Example</h2>
-      <PageMock>
-        {menuExample}
-      </PageMock>
-      <CodeRH
-        content={menuExample}
-      />
-      <h2 id="search">Search Example</h2>
-      <PageMock>
-        {searchExample}
-      </PageMock>
-      <CodeRH
-        content={searchExample}
       />
 
     </DocsContentPage>
